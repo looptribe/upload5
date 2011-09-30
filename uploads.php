daef86430f10531bb51c62601c2f0ba8aa92c1c4
ghidosoft <ghidosoft@gmail.com>
2011-09-30 15:17:38 +0200

    Example improved

--- uploads.php
+++ uploads.php
@@ -1,1 +1,13 @@
-{success: true}
+<?php
+    header('Content-Type: application/json');
+    $results = array('result' => false);
+    
+    if (isset($_FILES['file']))
+    {
+        $file = $_FILES['file'];
+        $results['result'] = $file['error'] == UPLOAD_ERR_OK;
+        $results['filename'] = $file['name'];
+        $results['size'] = $file['size'];
+    }
+        
+    echo json_encode($results);
